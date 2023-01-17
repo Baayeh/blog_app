14685a1c23398334bbec8016f5d3ec5b0f2321df
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-17 15:49:06 +0000

    Add method to update likes counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
   after_create :increment_comment_count
+  after_create :increment_like_count
 
   def most_recent_comments
     self.comments.order(created_at: :desc).limit(5)
@@ -11,5 +12,10 @@ class Post < ApplicationRecord
   def increment_comment_count
     self.increment!(:commentscounter)
   end
+
+  def increment_like_count
+    self.increment!(:likescounter)
+  end
+  
 
 end
