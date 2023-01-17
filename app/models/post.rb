c819e1e40c3325dcfc42c781e44147b350fbfb37
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-17 16:04:16 +0000

    Modify post model to increase the post counter in the user model

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,20 +2,21 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
-  after_create :increment_comment_count
+  after_create :increment_post_counter
   after_create :increment_like_count
 
   def most_recent_comments
     self.comments.order(created_at: :desc).limit(5)
   end
 
-  def increment_comment_count
-    self.increment!(:commentscounter)
+  def increment_post_counter
+    user.increment!(:postscounter)
   end
 
   def increment_like_count
     self.increment!(:likescounter)
   end
-  
 
+  private :increment_post_counter
+  
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,9 @@ class User < ApplicationRecord
   has_many :posts
   has_many :likes
   has_many :comments
-  after_create :increment_post_count
 
   def most_recent_posts
     self.posts.order(created_at: :desc).limit(3)
-  end
-
-  def increment_post_count
-    self.increment!(:postscounter)
   end
 
 end
