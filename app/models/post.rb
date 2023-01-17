7c2026353e4a3be59c51055914eb051d9b66cac9
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-17 15:48:20 +0000

    Add method to update comment counter

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,8 +2,14 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :likes
   has_many :comments
+  after_create :increment_comment_count
 
   def most_recent_comments
     self.comments.order(created_at: :desc).limit(5)
   end
+
+  def increment_comment_count
+    self.increment!(:commentscounter)
+  end
+
 end
