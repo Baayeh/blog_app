3a312f367c716b751f39802205533cb700c7ce48
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-18 05:32:14 +0000

    Add validation to the post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :likes
   has_many :comments
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :commentscounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+  validates :likescounter, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+
   after_create :increment_post_counter
 
   def most_recent_comments
