0b78d46b137e577dbead98186a0a60c907e2af26
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-21 16:29:14 +0000

    Add routes for the posts

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   # root "articles#index"
 
   shallow do
-    resources :users, only: [:index, :show]
+    resources :users, only: [:index, :show] do
+      resources :posts, only: [:index]
+    end
   end
 end
