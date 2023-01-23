4355cd5a1d140de69857821b4570e8e574bd63cc
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 22:16:20 +0000

    Display all posts for a particular user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,8 @@
 class PostsController < ApplicationController
-  def index; end
+  def index
+    @user = User.find(params[:user_id])
+    @user_posts = @user.posts
+  end
 
   def show
     @id = params['id']
