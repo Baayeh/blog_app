a09360a8416d4e86b0ceebc196aee905560df76b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 15:05:43 +0000

    Display details of a particular user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-  def index; end
   def index
     @users = User.all
   end
 
   def show
-    @id = params['id']
+    @user = User.find(params[:id])
+    @posts = @user.most_recent_posts
   end
 end
