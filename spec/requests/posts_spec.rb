6e1676cf10cafc81b84a793489206146b9394422
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 10:11:34 +0000

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
     before(:example) { get user_posts_path(1) }
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       expect(response).to render_template(:index)
     end
 
-    it "displays the correct HTML" do
-      expect(response.body).to include("Lists of Posts for a user")
+    it 'displays the correct HTML' do
+      expect(response.body).to include('Lists of Posts for a user')
     end
   end
 
   describe 'GET /show' do
     before(:example) { get user_post_path(1) }
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       expect(response).to render_template(:show)
     end
 
-    it "displays the correct HTML" do
-      expect(response.body).to include("Details for post 1")
+    it 'displays the correct HTML' do
+      expect(response.body).to include('Details for post 1')
     end
   end
 end
--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -4,32 +4,32 @@ RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
     before(:example) { get users_path }
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the index template" do
+    it 'renders the index template' do
       expect(response).to render_template(:index)
     end
 
-    it "displays the correct HTML" do
-      expect(response.body).to include("List of all users")
+    it 'displays the correct HTML' do
+      expect(response.body).to include('List of all users')
     end
   end
 
   describe 'GET /show' do
     before(:example) { get user_path(1) }
 
-    it "is a success" do
+    it 'is a success' do
       expect(response).to have_http_status(:ok)
     end
 
-    it "renders the show template" do
+    it 'renders the show template' do
       expect(response).to render_template(:show)
     end
 
-    it "displays the correct HTML" do
-      expect(response.body).to include("Details for user 1")
+    it 'displays the correct HTML' do
+      expect(response.body).to include('Details for user 1')
     end
   end
 end
