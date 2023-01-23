f2b665f0e55b7f70ccb210ca7f43c4dbc7823e5f
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 22:21:48 +0000

    Fix linter errors

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,9 +1,9 @@
 module UsersHelper
   def create_single_user(users, name)
-    render partial: name, locals: { obj: users } if !users.nil?
+    render partial: name, locals: { obj: users } unless users.nil?
   end
 
   def create_single_post(posts, name)
-    render partial: name, locals: { obj: posts } if !posts.nil?
+    render partial: name, locals: { obj: posts } unless posts.nil?
   end
 end
