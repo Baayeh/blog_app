ef4e5748b8c4aae5056d98f597a7cb6392e59f30
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 16:06:37 +0000

    Create a helper method for a single post

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -2,4 +2,8 @@ module UsersHelper
   def create_single_user(users, name)
     render partial: name, locals: { obj: users } if !users.nil?
   end
+
+  def create_single_post(posts, name)
+    render partial: name, locals: { obj: posts } if !posts.nil?
+  end
 end
