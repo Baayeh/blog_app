4132c9fc15fdd361e407cbdced79b5c83604dc69
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 09:02:28 +0000

    Add test to check response status for users#index action

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get users_path }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
