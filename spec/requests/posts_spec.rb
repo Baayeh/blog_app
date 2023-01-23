10ea13c4b6a4acb42d8bd16f82d279942ab9141b
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-23 10:01:11 +0000

    Add test to check response status for posts#index

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
   describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+    before(:example) { get user_posts_path(1) }
+
+    it "is a success" do
+      expect(response).to have_http_status(:ok)
+    end
   end
 end
