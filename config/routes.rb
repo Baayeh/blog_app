65140f582f97f6eaa02da11beeaa3854be807340
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2023-01-24 10:52:28 +0000

    Add resource route for the comments controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   shallow do
     resources :users, only: [:index, :show] do
-      resources :posts, only: [:index, :new, :create, :show]
+      resources :posts, only: [:index, :new, :create, :show] do
+        resources :comments, only: [:index, :create]
+      end
     end
   end
 
